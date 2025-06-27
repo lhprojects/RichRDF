138b503ccda3a092592e778176a8b1d8655a60cd
Harry Liang <lihawl@hotmail.com>
2025-06-27 18:04:14 +0200

    get_dependent_collection_names added

--- richrdf/__init__.py
+++ richrdf/__init__.py
@@ -1,7 +1,32 @@
 from .FlattenNpArray import  FlattenNpArray
 from .RichRDF import RichRDF
-def get_include_path():
-    import os
-    return os.path.join(os.path.dirname(__file__), "cpp_include")
+from .include import get_include_path
+
+
+def get_dependent_collection_names(filename, colls, printOnEachIter=False):
+    for iter in range(10000):  # try reading the event multiple times to catch all missing collections
+        try:
+            df = RichRDF("events", filename)
+            df = df.ReadEvent("evt", colls, throwExceptionOnRefCollIDNotFound=True)
+            df = df.Define("index", "evt.index")
+            df.Sum("index").GetValue()  # force reading the event
+            break
+        except Exception as e:
+            full_exception_name = f"{e.__class__.__module__}.{e.__class__.__name__}"
+            if full_exception_name != "cppyy.gbl.ana.exception_t":
+                raise
+
+            what = e.what()
+            import re
+            match = re.search(r"Collection (\w+) \(\d+\) not found", what)
+            if match:
+                missing_collection = match.group(1)
+                colls.append(missing_collection)
+            else:
+                raise  # rethrow if it's not a collection-not-found error
+        if printOnEachIter:
+            print("iter%2d: "%iter, colls)
+
+    return colls
 
 __all__ = ["include", "experimental"]  # Only expose these models
